b142ef2950e4d473d52d8607ebf7d9c5e1634003
KOLANICH <kolan_n@mail.ru>
2019-10-30 19:08:32 +0300

    Moved the metadata into setup.cfg

--- setup.py
+++ setup.py
@@ -1,48 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+from setuptools import setup
 
-import os
-from setuptools import setup, find_packages, Extension
-
-__version__ = "1.0rc1"
-
-
-def read(fname):
-    try:
-        with open(os.path.join(os.path.dirname(__file__), fname)) as fp:
-            return fp.read()
-    except IOError:
-        return ""
-
-setup(
-    name='dualscope123',
-    version=__version__,
-    packages=find_packages(),
-    package_data={
-      'dualscope123': []
-    },
-    install_requires=['numpy', 'pyaudio'],
-    entry_points = { 'console_scripts': [ 'dualscope123 = dualscope123.main:main', ], },
-    zip_safe=False,
-    include_package_data=True,
-    ext_modules=[Extension('dualscope123.probes.libethc', ['dualscope123/probes/ethc.c'])],
-    author="Giuseppe Venturini and others",
-    author_email="giuseppe.g.venturini@ieee.org",
-    description="A versatile oscilloscope + spectrum analyzer in Python/QWT",
-    long_description=''.join([read('pypi_description.rst'), '\n\n',
-                              read('CHANGES.rst')]),
-    license="GPLv3",
-    keywords="oscilloscope scope spectrum data",
-    url="http://github.com/ggventurini/dualscope123",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GPL v3 License",
-        "Operating System :: POSIX",
-        "Operating System :: POSIX :: Linux",
-        "Operating System :: MacOS",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7"]
-)
-
+if __name__ == "__main__":
+	setup(use_scm_version=True)
